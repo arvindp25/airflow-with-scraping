2ee8441a39468a88b5848eacff03791d444a1f39
arvindp25 <officialarvinop@gmail.com>
2021-10-23 09:11:56 -0700

    scraper code

--- __init__.py
+++ __init__.py
@@ -3,10 +3,95 @@ from airflow.operators.python_operators import PythonOperator
 from datetime import timedelta, datetime
 from scraping import get_player_info
 
+import requests
+from bs4 import BeautifulSoup as bs4
+
+
+
 def insert_gsheet():
     print("data inserted in gsheet")
 
-     
+
+
+def get_player_info(**kwargs):
+    offset = kwargs.get("offset", 60)
+    player_data = []
+    url = f"https://sofifa.com/players?offset={offset}"
+    p_html = requests.get(url)
+    p_soup = p_html.text
+    data = bs4(p_soup,'html.parser')
+    table = data.find('tbody')
+    for i in table.findAll('tr'):
+        data_dic = {}
+        td = i.findAll('td')
+        try:
+            data_dic["picture"] = td[0].find('img').get('data-src')
+        except:
+            pass
+        try:        
+            data_dic["ID"] = td[0].find('img').get('id')
+        except:
+            pass
+        try:
+
+            data_dic["flag"] = td[1].find('img').get('data-src')
+        except:
+            pass
+        try:        
+
+            data_dic["Name"] = td[1].find("a").text
+        except:
+            pass
+        try:        
+            
+            data_dic["Age"] = td[2].text.split()
+        except:
+            pass
+        try:
+            pos = td[1].find_all("span")
+            totl_pos = ""
+            for i in pos:
+                totl_pos += f", {i.text}"
+
+            data_dic["Position"] = totl_pos.strip(", ")
+        except:
+            pass
+        try:
+
+            data_dic["Overall"] = td[3].find('span').text
+        except:
+            pass
+        try:        
+            data_dic["Potential"] = td[4].find('span').text
+        except:
+            pass
+        try:        
+
+            data_dic["Team_image"] = td[5].find('img').get('data-src')
+        except:
+            pass
+        try:        
+            data_dic["Team"] = td[5].find('a').text
+        except:
+            pass
+        try:        
+            data_dic["Value"] = td[6].text.strip()
+        except:
+            pass
+        try:        
+            data_dic["Wage"] = td[7].text.strip()
+        except:
+            pass
+        try:        
+
+            data_dic["Total_Point"] = td[8].text.strip()
+        except:
+            pass
+        player_data.append(data_dic)
+
+        
+    return player_data
+
 
 
 default_args ={
